b1cd970d6a9fa27c248e71d0692951a49a4b07de
mahajan022 <anuragg7051@gmail.com>
2026-06-10 21:25:15 +0530

    manual sitemap fix

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import sitemap from 'vite-plugin-sitemap'
-
+ 
 export default defineConfig({
-  plugins: [
-    react(),
-    sitemap({
-      hostname: 'https://clicksnads.com',
-      routes: [
-        '/',
-        '/about',
-        '/services',
-        '/work',
-        '/contact',
-      ],
-    }),
-  ],
+  plugins: [react()],
 })
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
